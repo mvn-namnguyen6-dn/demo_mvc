e30793bd482390b6822ad6513c6d4b0e66fe4701
mvn-namnguyen6-dn <nam.nguyen6@monstar-lab.com>
2022-06-29 08:41:32 +0700

    upload

---
+++ views/posts/index.php
@@ -0,0 +1,10 @@
+<?php
+echo '<ul>';
+foreach ($posts as $post) {
+  echo '<li>
+    <a href="index.php?controller=posts&action=showPost&id=' . $post->id . '">' . $post->title . '</a>
+  </li>';
+}
+echo '</ul>';
+?>
+    
---
+++ views/posts/show.php
@@ -0,0 +1,6 @@
+
+<?php
+  echo "Tiêu đề:" . $data['post']->title;
+  echo "\n";
+  echo "Nội dung:" .$data['post']->content;
+?>
